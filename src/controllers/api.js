08ff50c7cbf5954dac1f866ae14f89fac44efa86
Darion Richards <darionrichards@outlook.com>
2021-12-03 01:19:25 +0000

    Delete a note from db.json
    - wrote deleteNote controller function

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const readFromDb = require("../utils/readFromDb");
+const writeToDb = require("../utils/writeToDb.js");
 
 const getNotes = (req, res) => {
     const notes = readFromDb();
@@ -9,7 +10,20 @@ const saveNote = (req, res) => {
     res.send("saveNote");
 };
 const deleteNote = (req, res) => {
-    res.send("deletNote");
+    const { id } = req.params;
+
+    const notes = readFromDb();
+    console.log(notes);
+
+    const newNotes = notes.filter((note) => {
+        return note.id !== id;
+    });
+
+    console.log(newNotes);
+
+    writeToDb(JSON.stringify(newNotes));
+
+    res.json(newNotes);
 };
 
 module.exports = {
